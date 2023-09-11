e337720c350332f98b414b077a4d664f8aa5aaff
Roderick Griffioen <roderick.griffioen@gmail.com>
2023-09-11 17:59:34 +0200

    Removed MAUI garbage

--- B738 System/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace B738_System;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
